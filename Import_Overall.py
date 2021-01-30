83614f1a302e94b81c902d5e777e93416c86e1a3
lbschmitz <lbschmitz@gmail.com>
2021-01-30 13:47:25 -0800

    Update Import_Overall.py

--- Import_Overall.py
+++ Import_Overall.py
@@ -1,3 +1,4 @@
+
 import json
 from datetime import date
 from qtrade import Questrade
@@ -5,13 +6,53 @@ from decimal import Decimal
 from forex_python.converter import CurrencyRates
 import mysql.connector
 from mysql.connector import Error
+import config 
+
+#---connect to questrade
 qtrade = Questrade(token_yaml='/home/ec2-user/token.yaml')
 qtrade.refresh_access_token(from_yaml=True)
 account_ids = qtrade.get_account_id()
 positions = []
+#---DONE
 
+#----Connect to DB
+db = mysql.connector.connect(
+    host=config.DATABASE_CONFIG['host'],
+    user=config.DATABASE_CONFIG['user'],
+    passwd=config.DATABASE_CONFIG['password'],
+    database=config.DATABASE_CONFIG['database']
+)
+mycursor = db.cursor()
+#----/Connect to DB
+
+#----Get info from Questrade
 
 for account in account_ids:
     print(account)
     temp = qtrade.get_account_positions(account_id=account)
     positions = positions + temp 
+#----/Get info flor Questrade
+#----Get Overal info
+c = CurrencyRates()
+Bookvalue = 0
+MarketValue = 0
+substring = '.TO'
+for item in positions:
+    if substring in item["symbol"]:
+        Bookvalue = Bookvalue + item["totalCost"] 
+        MarketValue = MarketValue + item["currentMarketValue"]
+    else:
+        Bookvalue = c.convert('USD', 'CAD', item['currentMarketValue']) + Bookvalue
+        MarketValue = c.convert('USD', 'CAD', item['currentMarketValue']) + MarketValue
+
+PL = MarketValue - Bookvalue
+#---------------------getday
+today = date.today()
+d1 = today.strftime("%Y/%m/%d")
+#---------------------import sql
+
+sqlstuff = "INSERT INTO Overall (Date, BookValue, MarketValue, MarketPL) VALUES (%s,%s,%s,%s)"
+record1 = (d1, Bookvalue, MarketValue, PL)
+mycursor.execute(sqlstuff, record1)
+db.commit()
+       
